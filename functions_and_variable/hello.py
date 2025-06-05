c38fa290a8817fc6537af62ff781f04511084c86
Dip lal <xaverianbarahi111@gmail.com>
2025-06-05 18:11:27 +0545

    python_first_code

---
+++ functions_and_variable/hello.py
@@ -0,0 +1,38 @@
+print("hello, Diplal")
+
+
+#-------> this sign represents comment that does not allow interpreter to run code.
+
+#for multiple line code
+"""
+This is multiple line commnets
+"""
+
+#dollar sign of compiler or interpreter imply where you are going to enter input
+
+#----------------------------------------------------------------------------------------
+#function
+#argument
+#parameter
+
+#----------------------------------------------------------------------------------------
+#bugs : they are mistakes in the code.
+#----------------------------------------------------------------------------------------
+#return values : it returns values to the function or a variables
+#variables: they are characters or string that can store values
+#----------------------------------------------------------------------------------------
+# = equal doesnot mean equality, it is assignment operator.
+#---------------------------------------------------------------------------------------
+
+#peudocode : it is overview of code how to write code.
+#---------------------------------------------------------------------------------------
+
+#concatenation
+name = input("what is ypur name?")
+print("my name is : " + name)
+
+#---------------------------------------------------------------------------------------
+name = input("what is ypur name?")
+print("my name is : ", name)
+
+#---------------------------------------------------------------------------------------
