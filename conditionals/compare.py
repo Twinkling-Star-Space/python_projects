6c9b2b60d3fba06e4f5c49b55bd7103f6ecfbfa9
Dip lal <xaverianbarahi111@gmail.com>
2025-06-06 23:17:44 +0545

    this was done after the end of vscode and kick off conditionals

---
+++ conditionals/compare.py
@@ -0,0 +1,13 @@
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+
+if(x<y):
+    print("The first number is less than the second number.")
+
+elif(x>y):
+    print("The first number is greater than the second number.")
+
+else:
+    print("The first number is equal to the second number.")
+
+  
